1e9eeaa1b6fbe7e26ea14e1f36b5d0ed96cf6784
Elliott Thoreby <ethoreby@gmail.com>
2014-04-22 14:24:24 -0700

    require shareJS

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var share = require('share');
+// var hat = require('hat');
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+var options = {
+  db: {type: 'none'},
+  browserChannel: {cors: '*'},
+  // auth: function(client, action) {}
+}
+share.server.attach(app, options);
 
 app.get('/', function(req, res) {
   res.render('index');
