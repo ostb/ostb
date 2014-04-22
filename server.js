c64e3989b045801126156fd40218fb60c6610430
Elliott Thoreby <ethoreby@gmail.com>
2014-04-22 14:01:11 -0700

    add ejs index

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.render('index');
 });
 
 app.listen(3000);
