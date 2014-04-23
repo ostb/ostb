40e34962335a291c45808fdcb1061ffa1cfc5c6c
Elliott Thoreby <ethoreby@gmail.com>
2014-04-22 19:02:47 -0700

    init creates a directory

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var share = require('share');
 // var hat = require('hat');
-// var git = require('nodegit');
+var git = require('nodegit');
 var app = express();
 
 app.set('views', __dirname + '/views');
@@ -17,6 +17,32 @@ share.server.attach(app, options);
 app.get('/', function(req, res) {
   console.log('recieved request');
   res.render('index');
+
+  var newRepo = new git.raw.Repo();
+  console.log(newRepo);
+
+  newRepo.init('./users/alejandro/', false, function(error, Repo) {
+    if (error) console.log(error);
+    console.log('created repo ', Repo);
+  });
 });
+
+  // git.repo('./users/alejandro/.git', function(error, repository) {
+  //   if (error) throw error;
+  //   console.log('retrieve repo: ', repository);
+
+  //   repository.init('./users/alejandro/', false, function(error, data) {
+  //     if (error) throw error;
+  //     console.log('created repo ', data);
+
+  //     git.repo('./users/alejandro/.git', function(error, repository) {
+  //       if (error) throw error;
+  //       console.log('user repo: ', repository);
+  //     });
+  //   });
+    
+  // });
+
+// });
 
 app.listen(3000);
