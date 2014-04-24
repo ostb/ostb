4d79a202565f38bd639a3d261bf07347fb043454
Elliott Thoreby <ethoreby@gmail.com>
2014-04-23 23:16:57 -0700

    example js-git create

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var share   = require('share');
 var Promise = require('bluebird');
+var git = require('git-node');
 
 var app = express();
 
@@ -15,27 +16,9 @@ var options = {
 }
 share.server.attach(app, options);
 
+// var example = require('git-node/examples/create');
+// var example = require('git-node/examples/read');
 
-var sys = require('sys')
-var exec = require('child_process').exec;
-var child;
- 
-// executes `pwd`
-// child = exec("pwd", function (error, stdout, stderr) {
-//   sys.print('stdout: ' + stdout);
-//   sys.print('stderr: ' + stderr);
-//   if (error !== null) {
-//     console.log('exec error: ' + error);
-//   }
-// });
-
-// exec("git init ~/users/" + "alejandro" + "/" + "intro_biology", function (error, stdout, stderr) {
-//   sys.print('stdout: ' + stdout);
-// });
-
-// exec("cd ~/users/" + "alejandro" + "/" + "intro_biology" + "&& git add --all && git commit", function (error, stdout, stderr) {
-//   sys.print('stdout: ' + stdout);
-// });
 
 app.get('/', function(req, res) {
   console.log('received request');
