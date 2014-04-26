e26781382447137558c26d58f76a0ce2fbec48db
Elliott Thoreby <ethoreby@gmail.com>
2014-04-26 16:50:04 -0700

    serve custom aceEditor script

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var share = require('share');
 // var hat = require('hat');
 var app = express();
 
-app.set('views', __dirname + '/client/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-// app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 var options = {
   db: {type: 'none'}
@@ -18,5 +18,12 @@ app.get('/', function(req, res) {
   console.log('recieved request');
   res.render('index');
 });
+
+// app.get('/lib/ace/ace.js', function(req, res) {
+//   // // res.set('Content-Type', 'text/javascript');
+//   // // res.sendfile('/lib/ace/ace');
+//   // res.sendfile('index');
+//   res.send('ace');
+// });
 
 app.listen(3000);
