781e8e270bad73376a7189d6ada5b0a8199aec43
Adrian Kim <uknoiluv@gmail.com>
2014-04-26 12:35:10 -0700

    aceEditor working

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.set('view engine', 'ejs');
 // app.use(express.static(__dirname + '/views'));
 
 var options = {
-  db: {type: 'none'},
-  browserChannel: {cors: '*'},
+  db: {type: 'none'}
+  // browserChannel: {cors: '*'},
   // auth: function(client, action) {}
 }
 share.server.attach(app, options);
