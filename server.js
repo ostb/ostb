5641e6b20131faef59246a52c5e58e4a6c64bff9
Adrian Kim <uknoiluv@gmail.com>
2014-04-27 15:52:47 -0700

    showdown markdown working added mustache lib to package.json

--- server.js
+++ server.js
@@ -1,7 +1,69 @@
 var express = require('express');
 var share = require('share');
+//adrian
+var showdown = require('showdown');
 // var hat = require('hat');
 var app = express();
+
+//adrian
+var converter = new showdown.converter();
+
+var Mustache = (function() {
+  try {
+    return require('mustache');
+  } catch (_error) {
+    e = _error;
+    return {
+      to_html: function() {
+        return "<body><pre>% npm install mustache</pre> to use this demo.";
+      }
+    };
+  }
+})();
+
+var render = function(content, name, docName, res) {
+  var html, markdown;
+  markdown = showdown.makeHtml(content);
+  html = Mustache.to_html(template, {
+    content: content,
+    markdown: markdown,
+    name: name,
+    docName: docName
+  });
+  res.writeHead(200, {
+    'content-type': 'text/html'
+  });
+  return res.end(html);
+};
+
+module.exports = function(docName, model, res) {
+  var name;
+  name = docName;
+  docName = "wiki:" + docName;
+  return model.getSnapshot(docName, function(error, data) {
+    if (error === 'Document does not exist') {
+      return model.create(docName, 'text', function() {
+        var content;
+        content = defaultContent(name);
+        return model.applyOp(docName, {
+          op: [
+            {
+              i: content,
+              p: 0
+            }
+          ],
+          v: 0
+        }, function() {
+          return render(content, name, docName, res);
+        });
+      });
+    } else {
+      return render(data.snapshot, name, docName, res);
+    }
+  });
+};
+
+
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -9,7 +71,6 @@ app.use(express.static(__dirname + '/public'));
 
 var options = {
   db: {type: 'none'}
-  // browserChannel: {cors: '*'},
   // auth: function(client, action) {}
 }
 share.server.attach(app, options);
