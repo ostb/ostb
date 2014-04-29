ef4a50145cc998f9bc97001324bb16aca65e79c2
Aria Bennett <twoplustwo@gmail.com>
2014-04-29 16:58:27 -0700

    Restructured project.

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
-angular.module('ostb', [
+var ostb = angular.module('ostb', [
   'ui.router',
   'factories.users',
   'factories.projects'
 ])
 
-.config( function($stateProvider) {
+.config(function($stateProvider) {
   console.log($stateProvider);
   $stateProvider
   .state('logout', {
@@ -43,132 +43,5 @@ angular.module('ostb', [
       }
     }
   })
-
-})
-
-.controller('VersionsController', function($scope) {
-  console.log('Versions controller');
-
-  $scope.versions = {
-    a: {
-      commitMsg: 'foo'
-    },
-    b: {
-      commitMsg: 'bar'
-    }
-  }
-})
-
-.controller('Example', function($scope) {
-  console.log('Example controller');
-})
-
-.controller('IndexContent', function($scope){
-  $scope.indexContent = 'test';
-})
-
-.directive('modalDialog', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      show: '='
-    },
-    replace: true, // Replace with the template below
-    transclude: true, // we want to insert custom content inside the directive
-    link: function(scope, element, attrs) {
-      scope.dialogStyle = {};
-      if (attrs.width)
-        scope.dialogStyle.width = attrs.width;
-      if (attrs.height)
-        scope.dialogStyle.height = attrs.height;
-      scope.hideModal = function() {
-        scope.show = false;
-      };
-    },
-    template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
-  };
-})
-
-.controller('ProjectsController', function($scope, ProjectsFactory) {
-  $scope.modalShown = false;
-  $scope.toggleModal = function() {
-    $scope.modalShown = !$scope.modalShown;
-  };
-
-  $scope.createProject = function(project) {
-    ProjectsFactory.create(project)
-    .then(function() {
-      console.log('success');
-    })
-    .catch(function(err) {
-      $scope.error = err;
-    });
-  };
-
-  $scope.cloneProject = function(project) {
-    ProjectsFactory.clone(project)
-    .then(function() {
-      console.log('success');
-    })
-    .catch(function(err) {
-      $scope.error = err;
-    });
-  };
-
-  $scope.deleteProject = function(project) {
-    ProjectsFactory.delete(project)
-    .then(function() {
-      console.log('success');
-    })
-    .catch(function(err) {
-      $scope.error = err;
-    });
-  };
-})
-
-.controller('UsersController', function($scope, UsersFactory) {
-  $scope.modalShown = false;
-  $scope.toggleModal = function() {
-    $scope.modalShown = !$scope.modalShown;
-  };
-
-  $scope.createUser = function(user) {
-    UsersFactory.create(user)
-    .then(function() {
-      console.log('success');
-    })
-    .catch(function(err) {
-      $scope.error = err;
-    });
-  };
-
-  $scope.deleteUser = function(user) {
-    UsersFactory.delete(user)
-    .then(function() {
-      console.log('success');
-    })
-    .catch(function(err) {
-      $scope.error = err;
-    });
-  };
-})
-
-
-// This is test data.
-window.documentsData = {
-  pages: {
-    1: {
-      name: "Mental Disorders of Coal Miners in Pre-Unification East Germany"
-    }
-  },
-  books: {
-    1: {
-      name: "Mining Coal Miners: Spelunking the Depths of The Coal Miner Psyche",
-      pages: {
-
-      }
-    }
-  }
-};
-console.log(window.documentsData);
+});
 
---
+++ public/controllers/controllers.js
@@ -0,0 +1,88 @@
+ostb.controller('IndexController', function($scope) {
+})
+
+.controller('UsersController', function($scope, UsersFactory) {
+  $scope.modalShown = false;
+  $scope.toggleModal = function() {
+    $scope.modalShown = !$scope.modalShown;
+  };
+
+  $scope.createUser = function(user) {
+    UsersFactory.create(user)
+    .then(function() {
+      console.log('success');
+    })
+    .catch(function(err) {
+      $scope.error = err;
+    });
+  };
+
+  $scope.deleteUser = function(user) {
+    UsersFactory.delete(user)
+    .then(function() {
+      console.log('success');
+    })
+    .catch(function(err) {
+      $scope.error = err;
+    });
+  };
+})
+
+.controller('VersionsController', function($scope) {
+  console.log('Versions controller');
+
+  $scope.versions = {
+    a: {
+      commitMsg: 'foo'
+    },
+    b: {
+      commitMsg: 'bar'
+    }
+  }
+})
+
+.controller('Example', function($scope) {
+  console.log('Example controller');
+})
+
+.controller('IndexContent', function($scope){
+  $scope.indexContent = 'test';
+})
+
+.controller('ProjectsController', function($scope, ProjectsFactory) {
+  $scope.modalShown = false;
+  $scope.toggleModal = function() {
+    $scope.modalShown = !$scope.modalShown;
+  };
+
+  $scope.createProject = function(project) {
+    ProjectsFactory.create(project)
+    .then(function() {
+      console.log('success');
+    })
+    .catch(function(err) {
+      $scope.error = err;
+    });
+  };
+
+  $scope.cloneProject = function(project) {
+    ProjectsFactory.clone(project)
+    .then(function() {
+      console.log('success');
+    })
+    .catch(function(err) {
+      $scope.error = err;
+    });
+  };
+
+  $scope.deleteProject = function(project) {
+    ProjectsFactory.delete(project)
+    .then(function() {
+      console.log('success');
+    })
+    .catch(function(err) {
+      $scope.error = err;
+    });
+  };
+});
+
