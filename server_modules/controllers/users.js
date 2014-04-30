c51599c88d16fd698ad342cdeb9969d59ad3388b
Elliott Thoreby <ethoreby@gmail.com>
2014-04-29 20:38:02 -0700

    deleteUser removes mongo record

--- server_modules/controllers/users.js
+++ server_modules/controllers/users.js
@@ -6,7 +6,6 @@ exports.create = function(req, res) {
   var db = req.db;
   var collection = db.get('usercollection');
 
-
   var newUser = Promise.promisify(shell.createUser);
   newUser(req.body.username)
   .then(function() {
@@ -22,9 +21,17 @@ exports.create = function(req, res) {
 }
 
 exports.delete = function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+
+  console.log(collection);
+
   shell.deleteUser(req.query.username)
   .then(function() {
     console.log('deleted user ', req.query.username);
+    collection.remove({
+      username: req.query.username
+    });
     res.send(204);
   })
   .catch(function(err){
