077a3a94545d7920e5a4c54d3e77d254fccd8cc3
Elliott Thoreby <ethoreby@gmail.com>
2014-04-29 22:14:20 -0700

    init creates mongo user record

--- server_modules/controllers/projects.js
+++ server_modules/controllers/projects.js
@@ -3,10 +3,30 @@ var shell = require('./../shell_commands');
 var bodyParser = require('body-parser');
 
 exports.create = function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+
   var newRepo = Promise.promisify(shell.init);
   newRepo(req.body.username, req.body.repo)
   .then(function() {
     console.log('created repo ', req.body.repo);
+
+    // var find = Promise.promisify(collection.findOne);
+    // find({username: req.body.username})
+    // .then(function(result) {
+    //   var projects = result.projects;
+    //   projects[req.body.repo] = {};
+      collection.update({username: req.body.username}, {$set: {'projects.foo': 'foo'}});
+      
+      var project = {};
+      project[req.body.repo] = {};
+      collection.update({username: req.body.username}, {$set: {projects: project}});
+
+    // })
+    // .catch(function(err) {
+    //   res.send(400, err.toString())
+    // })
+
     res.send(201);
   })
   .catch(function(err){
